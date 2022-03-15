76a496e78f89a97c230bd68b166016d873f4a10c
heechankim <heechan.dev@gmail.com>
2022-03-15 05:28:53 +0000

    Create public, private network and vpc

---
+++ prac2-vpc/vpc.tf
@@ -0,0 +1,30 @@
+resource "aws_vpc" "main" {
+	cidr_block = "10.0.0.0/16"
+	
+	tags = {
+		Name = "terraform-practice"
+	}
+}
+
+resource "aws_subnet" "public_subnet" {
+	vpc_id = aws_vpc.main.id
+	cidr_block = "10.0.0.0/24"
+
+	availability_zone = "ap-northeast-2a"
+
+	tags = {
+		Name = "terraform-practice-public-subnet"
+	}
+}
+
+resource "aws_subnet" "private_subnet" {
+	vpc_id = aws_vpc.main.id
+	cidr_block = "10.0.1.0/24"
+
+	tags = {
+		Name = "terraform-practice-private-subnet"
+	}
+}
+
+
+
