1403be8a9fa831eef72a7b7eed5e2d12c42f595f
heechankim <heechan.dev@gmail.com>
2022-03-15 06:13:57 +0000

    Fix: } missing

--- prac2-vpc/vpc.tf
+++ prac2-vpc/vpc.tf
@@ -32,7 +32,23 @@ resource "aws_internet_gateway" "igw" {
 	tags = {
 		Name = "terraform-practice-igw"
 	}
+}
 
+resource "aws_eip" "nat" {
+	vpc = true
 
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+	allocation_id = aws_eip.nat.id
+
+	subnet_id = aws_subnet.public_subnet.id
+	
+	tags = {
+		Name = "terraform-natgw"
+	}
 }
 
