da89c7e365600237483a3dd4a32b5bdd22d38d9c
heechankim <heechan.dev@gmail.com>
2022-03-15 06:20:08 +0000

    Create: route tables

--- prac2-vpc/vpc.tf
+++ prac2-vpc/vpc.tf
@@ -52,3 +52,28 @@ resource "aws_nat_gateway" "nat_gateway" {
 	}
 }
 
+resource "aws_route_table" "public" {
+	vpc_id = aws_vpc.main.id
+
+	tags = {
+		Name = "terraform-rt-public"
+	}
+}
+
+resource "aws_route_table_association" "route_table_association_public" {
+	subnet_id = aws_subnet.public_subnet.id
+	route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+	vpc_id = aws_vpc.main.id
+
+	tags = {
+		Name = "terraform-rt-private"
+	}
+}
+
+resource "aws_route_table_association" "route_table_association_private" {
+	subnet_id = aws_subnet.private_subnet.id
+	route_table_id = aws_route_table.private.id
+}
