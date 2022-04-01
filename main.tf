fa793d52211f05dec4d7eacfcfb7411ced4d7e47
heechankim <heechan.dev@gmail.com>
2022-04-02 00:02:24 +0900

    Add: single instance

--- main.tf
+++ main.tf
@@ -2,4 +2,8 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-
+resource "aws_instance" "example" {
+  ami = "ami-063454de5fe8eba79"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-00589220d728d28b3"
+}
