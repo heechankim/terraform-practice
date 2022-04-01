caaf712c1510b90340440834be9a52ff9b7441ff
heechankim <heechan.dev@gmail.com>
2022-04-02 00:09:22 +0900

    Add: resource tag

--- main.tf
+++ main.tf
@@ -6,4 +6,8 @@ resource "aws_instance" "example" {
   ami = "ami-063454de5fe8eba79"
   instance_type = "t2.micro"
   subnet_id = "subnet-00589220d728d28b3"
+
+  tags = {
+    Name = "terraform-example"
+  }
 }
