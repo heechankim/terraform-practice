833dbe885a81e7fbcc2c51f0db28ef64b5e246f7
heechankim <heechan.dev@gmail.com>
2022-04-03 21:23:09 +0900

    Create: new security groups with vpc_id Change: instance's security group

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
+  vpc_id = "vpc-0bef10d3279fdf951"
+
   ingress {
     from_port = 8080
     to_port = 8080
@@ -17,6 +19,8 @@ resource "aws_instance" "example" {
   ami = "ami-063454de5fe8eba79"
   instance_type = "t2.micro"
   subnet_id = "subnet-00589220d728d28b3"
+  
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                   #!/bin/bash
