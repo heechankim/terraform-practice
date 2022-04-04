12dc75d11f989652fa4bc7adee77cc0f00701907
heechankim <heechan.dev@gmail.com>
2022-04-04 21:41:38 +0900

    Modified: nohub -> nohup

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-  vpc_id = "vpc-0bef10d3279fdf951"
+  vpc_id = "vpc-03c1e8a33ab568b4f"
 
   ingress {
     from_port = 8080
@@ -18,7 +18,7 @@ resource "aws_security_group" "instance" {
 resource "aws_instance" "example" {
   ami = "ami-063454de5fe8eba79"
   instance_type = "t2.micro"
-  subnet_id = "subnet-00589220d728d28b3"
+  subnet_id = "subnet-02abe304152c47909"
   
   vpc_security_group_ids = [aws_security_group.instance.id]
 
@@ -27,7 +27,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
                   #!/bin/bash
                   echo "Hello, World!!" > index.html
-                  nohub busybox httpd -f -p 8080 &
+                  nohup busybox httpd -f -p 8080 &
                   EOF
 
   tags = {
