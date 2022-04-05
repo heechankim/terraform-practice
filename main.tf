7dcba704046332c5ac989f428cf20edab23c5f1b
heechankim <heechan.dev@gmail.com>
2022-04-05 22:06:50 +0900

    Add: Asg configuration

--- main.tf
+++ main.tf
@@ -2,22 +2,20 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-variable "server_name" {
-  description = "The name of web server"
-  type = string
-  default = "terra-example"
-}
-
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type = number
   default = 8080
 }
 
-resource "aws_security_group" "instance" {
-  name = "${var.server_name}"
+data "aws_vpc" "default_vpc" {
+  default = true
+}
 
-  vpc_id = "vpc-03c1e8a33ab568b4f"
+resource "aws_security_group" "instance" {
+  name = "sg-terra-example"
+
+  vpc_id = data.aws_vpc.default.id 
 
   ingress {
     from_port = var.server_port
@@ -27,27 +25,32 @@ resource "aws_security_group" "instance" {
   }
 }
 
-resource "aws_instance" "terra-example" {
-  ami = "ami-063454de5fe8eba79"
+resource "aws_launch_configuration" "terra-example" {
+  image_id = "ami-063454de5fe8eba79"
   instance_type = "t2.micro"
-  subnet_id = "subnet-02abe304152c47909"
   
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  associate_public_ip_address = true
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                   #!/bin/bash
-                  echo "Hello, World!!\n${var.server_name}" > index.html
+                  echo "<h1>Hello, World!!</h1>" > index.html
                   nohup busybox httpd -f -p ${var.server_port} &
                   EOF
-
-  tags = {
-    Name = "${var.server_name}"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-output "server_ip" {
-  value = aws_instance.terra-example.public_ip
-  description = "The Public IP address of the webserver"
+resource "aws_autoscaling_group" "terra-example-asg" {
+  launch_configuration = aws_launch_configuration.terra-example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terra-example-asg"
+    propagate_at_launch = true
+  }
 }
+
