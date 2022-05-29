f7bab1386ad913be1822db5e3941c06272bc852d
heechankim <heechan.dev@gmail.com>
2022-05-29 14:11:37 +0000

    Create: asg and launch configuration (p.80)

--- main.tf
+++ main.tf
@@ -2,33 +2,63 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+variable "vpc-id" {
+  description = "The VPC's id"
+  type = string
+  default = "vpc-0987dbd997f01ea0e"
+}
+
+variable "public-subnet1" {
+  type = string
+  default = "subnet-0e7e882226867dcb6"
+}
+
+variable "public-subnet2" {
+  type = string
+  default = "subnet-08a3d30fa40e441fd"
+}
+
 variable "server_port" {
   description = "The Port the server will use for HTTP requests"
   type = number
   default = 8080
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-063454de5fe8eba79"
+resource "aws_launch_configuration" "example" {
+  image_id = "ami-063454de5fe8eba79"
   instance_type = "t2.micro"
-  subnet_id = "subnet-0e7e882226867dcb6" 
-  associate_public_ip_address = true
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World!!" > index.html
+              echo "<h1>Hello, World!!</h1><br><br><hr>" > index.html
+              echo "Host: `uname -a`" >> index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
-  tags = {
-    Name = "terraform-example"
+
+  lifecycle {
+    create_before_destroy = true
   }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 2
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+  vpc_zone_identifier = [var.public-subnet1, var.public-subnet2]
 }
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-  vpc_id = "vpc-0987dbd997f01ea0e" 
+  vpc_id = var.vpc-id 
 
   ingress {
     from_port = var.server_port
@@ -38,7 +68,3 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  description = "The public IP address of the web server"
-  value = aws_instance.example.public_ip
-}
