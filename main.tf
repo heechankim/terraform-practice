4e376459dc79a86338f6ac3a05e3d27ea2b96f97
heechankim <heechan.dev@gmail.com>
2022-05-29 12:38:28 +0000

    Chore: 8080 connection refused

--- main.tf
+++ main.tf
@@ -2,30 +2,41 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_default_vpc" "default" {
-  tags = {
-    Name = "default-vpc"
-  }
-
-  force_destroy = true
-}
-
-resource "aws_default_subnet" "default" {
-  tags = {
-    Name = "default-subnet"
-  }
-
-  availability_zone = "ap-northeast-2a"
-  force_destroy = true
-}
-
 resource "aws_instance" "example" {
   ami = "ami-063454de5fe8eba79"
   instance_type = "t2.micro"
+  subnet_id = "subnet-0e7e882226867dcb6" 
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "terraform-example"
   }
-  subnet_id = aws_default_subnet.default.id
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  vpc_id = "vpc-0987dbd997f01ea0e" 
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
